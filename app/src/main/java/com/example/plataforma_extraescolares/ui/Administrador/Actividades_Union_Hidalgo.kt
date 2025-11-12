96101f43738bd97cd0557d1a0aaaa03e72629ea8
Vasquezivan <ivanvasquezcrry30@gmail.com>
2025-11-12 17:21:41 -0600

    Panel Principal Unión Hidalgo
    Se desarrollo el panel principal de la unidad académica de unión hidalgo.

---
+++ app/src/main/java/com/example/plataforma_extraescolares/ui/Administrador/Actividades_Union_Hidalgo.kt
@@ -0,0 +1,23 @@
+package com.example.plataforma_extraescolares.ui.Administrador
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.plataforma_extraescolares.R
+
+class Actividades_Union_Hidalgo : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_actividades_union_hidalgo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
